6c5dc0a1260a2a18b6ed0adacd25dff1646b257e
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-03-18 10:45:56 +0530

    first commit

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -21,13 +21,26 @@ public class BlogService {
     @Autowired
     UserRepository userRepository1;
 
-    public Blog createAndReturnBlog(Integer userId, String title, String content) {
+    public Blog createAndReturnBlog(Integer userId, String title, String content) throws Exception{
         //create a blog at the current time
+        User user;
+        try {
+            user = userRepository1.findById(userId).get();
+        }catch (Exception e){
+            throw new Exception("User does not exists");
+        }
+
+
+        Blog blog = new Blog(title,content,user);
+        user.getBlogList().add(blog);
+        userRepository1.save(user);
+
+        return blog;
 
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
-
+        blogRepository1.deleteById(blogId);
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -15,17 +15,42 @@ public class ImageService {
     @Autowired
     ImageRepository imageRepository2;
 
-    public Image addImage(Integer blogId, String description, String dimensions){
+    public Image addImage(Integer blogId, String description, String dimensions) throws Exception{
         //add an image to the blog
+        Blog blog;
+        try{
+            blog = blogRepository2.findById(blogId).get();
+        }catch(Exception e){
+            throw new Exception("Blog does not exists");
+        }
+
+        Image image = new Image(description,dimensions,blog);
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+
+        return image;
 
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
-    public int countImagesInScreen(Integer id, String screenDimensions) {
+    public int countImagesInScreen(Integer id, String screenDimensions) throws Exception{
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Image image;
+        try{
+            image = imageRepository2.findById(id).get();
+        }catch(Exception e){
+            throw new Exception("Image does not exists");
+        }
 
+        String[] screenDim = screenDimensions.split("X");
+        String[] imageDim = image.getDimensions().split("X");
+
+        int screenArea = Integer.parseInt(screenDim[0]) * Integer.parseInt(screenDim[1]);
+        int imageArea = Integer.parseInt(imageDim[0]) * Integer.parseInt(imageDim[1]);
+
+        return screenArea/imageArea;
     }
 }
